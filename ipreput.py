59f31fc132a29fceeedd4ac26c60c742c2136925
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2022-11-06 10:55:36 +0300

    Add files via upload

---
+++ ipreput.py
@@ -0,0 +1,61 @@
+import json
+import csv
+import requests
+import sys
+import argparse
+
+
+#Twitter:@r00t_nasser
+def clear():
+	json = open("result/result.json","w")
+	json.close()
+	excel = open("result/result.csv","w")
+	excel.close()
+def temp():
+	temp = open("temp/temp.json","w")
+	temp.close()
+
+def neutrinoapi():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", required=True)
+	args = parser.parse_args()
+	temp = 0
+	ips = open('ips.txt', 'r')
+	csv_file = "result/result.csv"
+	for ip in ips:
+		url = 'https://api.abuseipdb.com/api/v2/check'
+		querystring = {
+		'ipAddress': ip,
+		'maxAgeInDays': '90'
+		}
+		headers = {
+        'Accept': 'application/json',
+        'Key': '0e66f143f8d6daadbc3ccf489e57d5829d5b5f9cf245a46ec3b01fde086765c54d841365e4c4a010'
+        }
+		response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+		decodedResponse = json.loads(response.text)
+		print(json.dumps(decodedResponse, sort_keys=True, indent=4),file=open('result/result.json', 'a'))
+		print(json.dumps(decodedResponse, sort_keys=True, indent=4),file=open('temp/temp.json', 'w'))
+		f = open('temp/temp.json')
+		data = json.load(f)
+		
+		for key, value in data.items():
+			dic = [value]
+		csv_columns = ['abuseConfidenceScore','countryCode','domain','hostnames','ipAddress','ipVersion','isPublic','isWhitelisted','isp','lastReportedAt','numDistinctUsers','totalReports','usageType']
+		
+		try:
+			with open(csv_file, 'a') as csvfile:
+				
+				writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+				if temp == 0:
+					writer.writeheader()
+				temp += 1
+				for data in dic:
+					print(data)
+					writer.writerow(data)
+		
+		except IOError:
+			print("I/O error")
+clear()
+neutrinoapi()
+temp()
